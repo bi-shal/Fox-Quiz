b3f6ea17e52899f78ad8ba97547a0ae7b3c415b3
bi-shal <bishalsarkar336@gmail.com>
2022-10-12 02:41:44 +0600

    statics-done

--- src/component/Chart/Statics.js
+++ src/component/Chart/Statics.js
@@ -1,14 +1,10 @@
 import React, { useContext } from 'react';
-import { useLoaderData } from 'react-router-dom';
-// import { Line, LineChart } from 'recharts';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, Tooltip } from 'recharts';
 import { ProductContext } from '../../Layout/Main';
 
 
 const Statics = () => {
     const products = useContext(ProductContext)
-    // const products = useLoaderData()
-    // console.log(products);
     
     return (
         <div>
